5e66da6228efd245228c345ca24c7f56a5bf3564
MohanRaj <mraj86698@gmail.com>
2022-09-19 17:01:43 +0530

    Added to Retrieve the Employee Payroll from database

--- ParyrollService/src/main/java/com/java/maven/ParyrollService/PayrollServiceMain.java
+++ ParyrollService/src/main/java/com/java/maven/ParyrollService/PayrollServiceMain.java
@@ -5,6 +5,8 @@ public class PayrollServiceMain
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Employee Payroll Service!" );
+        PayrollService service = new PayrollService();
+    	service.getEmpData();
 
     }
 }
