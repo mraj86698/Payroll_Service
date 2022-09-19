1e92732fa2ecdcd1edd6a88fb16bcd3761e693a8
MohanRaj <mraj86698@gmail.com>
2022-09-19 15:36:14 +0530

    Added to Welcome Message

---
+++ ParyrollService/src/main/java/com/java/maven/ParyrollService/PayrollServiceMain.java
@@ -0,0 +1,9 @@
+package com.java.maven.ParyrollService;
+
+public class PayrollServiceMain
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Employee Payroll Service!" );
+    }
+}
---
+++ ParyrollService/src/test/java/com/java/maven/ParyrollService/PayrollServiceMainTest.java
@@ -0,0 +1,15 @@
+package com.java.maven.ParyrollService;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class PayrollServiceMainTest
+{
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
